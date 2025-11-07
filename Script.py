3030a2d7a0cb0a6751ecd9e659eb49a26350b2d5
Lucky7496 <luckyroy7496@gmail.com>
2025-11-08 00:09:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/moviesgrouptelugu">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/Lucky_2812">[ Lucky_2812 ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
